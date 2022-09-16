b815d33c343dc9757f695cf70c750c61aaf4d728
TheWalkingKingPro <98580090+TheWalkingKingPro@users.noreply.github.com>
2022-09-16 12:54:11 +0530

    Create app2.js

---
+++ app2.js
@@ -0,0 +1,60 @@
+function App() {
+        const [error, setError] = useState(null);
+        const [isLoaded, setIsLoaded] = useState(false);
+        const [items, setItems] = useState([]);
+
+        useEffect(() => {
+            fetch("Link to fetch the API")
+                .then((res) => res.json())
+                .then(
+                    (result) => {
+                        setIsLoaded(true);
+                        setItems(result);
+                    },
+                    
+              (error) => {
+                        setIsLoaded(true);
+                        setError(error);
+                    }
+                );
+        }, []);
+
+        if (error) {
+            return <>{error.message}</>;
+        } else if (!isLoaded) {
+            return <>loading...</>;
+        } else {
+            return (
+                <div className="wrapper">
+                    <ul className="card-grid">
+                        {items.map((item) => (
+                            <li>
+                                <article className="card" key={item.callingCodes}>
+                                    <div className="card-image">
+                                        <img src={item.flag} alt={item.name} />
+                                    </div>
+                                    <div className="card-content">
+                                        <h2 className="card-name">{item.name}</h2>
+                                        <ol className="card-list">
+                                            <li>
+                                                population:{" "}
+                                                <span>{item.population}</span>
+                                            </li>
+                                            <li>
+                                                Region: <span>{item.region}</span>
+                                            </li>
+                                            <li>
+                                                Capital: <span>{item.capital}</span>
+                                            </li>
+                                        </ol>
+                                    </div>
+                                </article>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            );
+        }
+    }
+
+    ReactDOM.render(<App />, document.getElementById("root"));
