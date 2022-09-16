1e6a5840ba5708e4f062d018c35b33b2d0ef4e14
TheWalkingKingPro <98580090+TheWalkingKingPro@users.noreply.github.com>
2022-09-16 13:01:16 +0530

    Create app4.js

---
+++ app4.js
@@ -0,0 +1,52 @@
+   return <>{error.message}</>;
+        } else if (!isLoaded) {
+            return <>loading...</>;
+        } else {
+            return (
+                <div className="wrapper">
+                    <div className="search-wrapper">
+                        <label htmlFor="search-form">
+                            <input
+                                type="search"
+                                name="search-form"
+                                id="search-form"
+                                className="search-input"
+                                placeholder="Search for..."
+                                value={q}
+                                onChange={(e) => setQ(e.target.value)}
+                            />
+                            <span className="sr-only">Search countries here</span>
+                        </label>
+                    </div>
+                    <ul className="card-grid">
+                        {items.map((item) => (
+                            <li>
+                                <article className="card" key={item.callingCodes}>
+                                    <div className="card-image">
+                                        <img src={item.flag} alt={item.name} />
+                                    </div>
+                                    <div className="card-content">
+                                        <h2 className="card-name">{item.name}</h2>
+                                        <ol className="card-list">
+                                            <li>
+                                                population:{" "}
+                                                <span>{item.population}</span>
+                                            </li>
+                                            <li>
+                                                Region: <span>{item.region}</span>
+                                            </li>
+                                            <li>
+                                                Capital: <span>{item.capital}</span>
+                                            </li>
+                                        </ol>
+                                    </div>
+                                </article>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            );
+        }
+    }
+
+    ReactDOM.render(<App />, document.getElementById("root"));
